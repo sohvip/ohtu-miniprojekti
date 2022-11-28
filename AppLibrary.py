f50c59a626f7e2744792a1f86a2c5f5b2027e6de
emma <emmasofia@live.fi>
2022-11-28 16:00:50 +0200

    setting up tests

--- AppLibrary.py
+++ AppLibrary.py
@@ -9,3 +9,17 @@ class AppLibrary:
 
     def reset_application(self):
         requests.post(f"{self._base_url}/tests/reset")
+
+    
+    def create_book(self, identifier, author, editor, title, publisher, year):
+        data = {
+            "identifier": identifier,
+            "author": author,
+            "editor": editor,
+            "title": title,
+            "publisher": publisher,
+            "year": year
+
+        }
+
+        requests.post(f"{self._base_url}", data=data)
