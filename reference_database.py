e5fca227049218636a77e183c68836d62c2b9bef
Rasmus S <rasmus.salmela@gmail.com>
2022-11-28 14:57:09 +0200

    Add book uniqueness check

--- reference_database.py
+++ reference_database.py
@@ -5,6 +5,11 @@ def create_book(identifier, author, editor, title, publisher, year):
 
     if identifier_already_exists(identifier):
         return "Identifier already in use for another work"
+
+    book_exists = already_exists_book()
+    if book_exists is not None:
+        return "Identicial book already exists already with identifier " + book_exists
+
     
     sql = "INSERT INTO books (ref_type, identifier, author, editor, title, publisher, year) VALUES (book, :id, :auth, :edit, :title, :publ, :year)"
     db.session.execute(sql, {"id":identifier, "auth":author, "edit":editor, "title":title, "publ":publisher, "year":year})
@@ -18,7 +23,7 @@ def identifier_already_exists(identifier):
     return False
 
 def identifier_already_exists_books(identifier):
-    sql = "SELECT FROM books WHERE identifier=:id"
+    sql = "SELECT FROM * books WHERE identifier=:id"
     result = db.session.execute(sql, {"id":identifier})
     result = result.fetchone()
 
@@ -26,3 +31,14 @@ def identifier_already_exists_books(identifier):
         return True
 
     return False
+
+def already_exists_book(author, editor, title, publisher, year):
+
+    sql = "SELECT identifier FROM books WHERE author:=a, editor:=e, title:=t, publisher:=p, year=:y"
+    result = db.session.execute(sql, {"a":author, "e":editor, "t":title, "p":publisher, "y":year})
+    result = result.fetchone()[0]
+
+    if result is not None:
+        return result
+
+    return None
