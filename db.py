d816d7f66765ba4e57b3a34e89a1aa76c9616a3f
Sohvi Puska <sohvip1@gmail.com>
2022-11-28 16:03:00 +0200

    tietokanta

--- db.py
+++ db.py
@@ -1,1 +1,7 @@
-# tshoa-like database connection here
+from os import getenv
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
