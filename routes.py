8737106a5bd2b50114c7429370fdbb7ba4c49833
mirelle01 <lybackmirelle@gmail.com>
2022-11-28 15:35:24 +0200

    give index.html list of saved books

--- routes.py
+++ routes.py
@@ -1,11 +1,13 @@
 from app import app
 from reference_database import create_book
+from printing_stuff import get_books_human_readable
 from flask import request, render_template, redirect
 
 @app.route('/', methods=['get', 'post'])
 def index():
+    book_list = get_books_human_readable()
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', book_list=book_list)
     if request.method == 'POST':
         identifier = request.form['identifier']
         author = request.form['author']
