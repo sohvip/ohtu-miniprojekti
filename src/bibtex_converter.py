ef3ce99c48604237947c941f9dda8f00232cd7cd
mirelle01 <lybackmirelle@gmail.com>
2022-12-05 16:32:23 +0200

    database tests

--- src/bibtex_converter.py
+++ src/bibtex_converter.py
@@ -1,6 +1,6 @@
-from reference_database import get_book
+import reference_database
 
 def book_to_bibtex(id):
-    book = get_book(id)
+    book = reference_database.get_book(id)
     bibtex = f'@book{{{book[0]}, author = \"{book[1]}\", title = \"{book[2]}\", publisher = \"{book[3]}\", address = \"{book[4]}\", year = {book[5]}}}'
     return bibtex
