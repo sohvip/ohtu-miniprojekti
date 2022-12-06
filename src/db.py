07037fe4d7b534e2cba59e78cda7813d81765899
mirelle01 <lybackmirelle@gmail.com>
2022-12-06 12:19:15 +0200

    db.py

--- src/db.py
+++ src/db.py
@@ -2,6 +2,6 @@ from os import getenv
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///psohvi" #getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///mirelle" #getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
