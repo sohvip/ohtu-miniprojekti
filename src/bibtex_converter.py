32c290fbc04f44d61533ad4289df27c6413ecc8e
Sohvi Puska <sohvip1@gmail.com>
2022-12-06 12:24:07 +0200

    bibtex

--- src/bibtex_converter.py
+++ src/bibtex_converter.py
@@ -3,5 +3,5 @@ import reference_database
 
 def book_to_bibtex(id):
     book = reference_database.get_book(id)
-    bibtex = f'@book{{{book[0]}, author = \"{book[1]}\", title = \"{book[2]}\", publisher = \"{book[3]}\", address = \"{book[4]}\", year = {book[5]}}}'
+    bibtex = f'@book{{{book[0]}, author = \"{book[1]}\", editor = \"{book[2]}\", title = \"{book[3]}\", publisher = \"{book[4]}\", year = {book[5]}}}'
     return bibtex
