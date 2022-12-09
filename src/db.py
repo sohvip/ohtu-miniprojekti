61f3a6307eab7becdd8ec2e3cf707bc9cca23ccc
Nicolas <kivimakinicolas@gmail.com>
2022-12-09 14:03:07 +0200

    db fix

--- src/db.py
+++ src/db.py
@@ -1,8 +1,23 @@
-from os import getenv
+import os
 from flask_sqlalchemy import SQLAlchemy
 from app import app
+from dotenv import load_dotenv
 
+dirname = os.path.dirname(__file__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+except FileNotFoundError:
+    pass
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+ENV = os.getenv("FLASK_ENV") or "production"
+
+if DATABASE_URL is None:
+    raise Exception(
+        f"Database URI is not defined with the DATABASE_URL environment variable"
+    )
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
